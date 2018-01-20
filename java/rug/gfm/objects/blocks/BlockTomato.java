bd85659dad0e2ddd4e53c5f72a1c230f7ec8c55a
Noah Price <noah.price.237@gmail.com>
2018-01-19 19:54:04 -0500

    not sure

--- java/rug/gfm/objects/blocks/BlockTomato.java
+++ java/rug/gfm/objects/blocks/BlockTomato.java
@@ -6,20 +6,23 @@ import net.minecraft.block.state.IBlockState;
 
 public class BlockTomato extends Block{
 
+<<<<<<< HEAD
 	//public static final AxisAlignedBB BLOCK_TOMATO_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.9375D, 0.9375D);
 	
 	public BlockTomato(String name) {
 		super(Material.GOURD);
-	}
+=======
+	public static final AxisAlignedBB BLOCK_TOMATO_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.9375D, 0.9375D);
 	
-	@Override
-	public boolean isFullCube(IBlockState state) {
-        return false;
-    }
+	public BlockTomato(String name, Material gourd) {
+		super(name, Material.GOURD);
+>>>>>>> 352996ba11167d931e2a94fe7edb047e073716d1
+	}
 	
 	@Override
     public boolean isOpaqueCube(IBlockState state) {
         return false;
+<<<<<<< HEAD
     }
 
 	
@@ -28,5 +31,15 @@ public class BlockTomato extends Block{
 		//return BLOCK_TOMATO_AABB;
 		
 	//}
+=======
+		
+	}
+	
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		return BLOCK_TOMATO_AABB;
+		
+	}
+>>>>>>> 352996ba11167d931e2a94fe7edb047e073716d1
 	
 }

21aa76733b0946219d095869a66833a8359db2a5
Noah Price <noah.price.237@gmail.com>
2018-01-19 19:33:23 -0500

    Update to new Github for Windows
    as

--- java/rug/gfm/objects/blocks/BlockTomato.java
+++ java/rug/gfm/objects/blocks/BlockTomato.java
@@ -1,17 +1,15 @@
 package rug.gfm.objects.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
 
-public class BlockTomato extends BlockBase{
+public class BlockTomato extends Block{
 
-	public static final AxisAlignedBB BLOCK_TOMATO_AABB = new AxisAlignedBB(0.25D, 0.25D, 0.25D, 0.25D, 0.25D, .5D);
+	//public static final AxisAlignedBB BLOCK_TOMATO_AABB = new AxisAlignedBB(0.0625D, 0.0D, 0.0625D, 0.9375D, 0.9375D, 0.9375D);
 	
 	public BlockTomato(String name) {
-		super(name, Material.GOURD);
+		super(Material.GOURD);
 	}
 	
 	@Override
@@ -23,11 +21,12 @@ public class BlockTomato extends BlockBase{
     public boolean isOpaqueCube(IBlockState state) {
         return false;
     }
+
 	
-	
-	@Override
-	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-		return BLOCK_TOMATO_AABB;
+	//@Override
+	//public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		//return BLOCK_TOMATO_AABB;
 		
-	}
+	//}
+	
 }
